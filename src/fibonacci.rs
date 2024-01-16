025e9a498799fc2c34d4ca90a30c5a5bcff6c18a
Mello Birkan <mgbirkan@icloud.com>
2024-01-16 14:08:33 -0300

    Update 'fibonacci' function and add 'fibs' function in fibonacci.rs
    This commit modifies the 'fibonacci' function to take and return unsigned integers instead of signed. Additionally, a new function 'fibs' has been added, which generates a vector of the first 'n' numbers of the Fibonacci sequence by using the updated 'fibonacci' function.

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,7 +1,16 @@
-pub fn fibonacci(term: i32) -> i32 {
+pub fn fibonacci(term: u32) -> u32 {
     match term {
         0 =>  0,
         1 =>  1,
         _ => fibonacci(term-1) + fibonacci(term-2),
     }
+}
+
+// esse programa retorna um vetor com os n primeiros numeros da sequencia de fibonacci {
+fn fibs(count: usize) -> Vec<u32> {
+    let mut fib: Vec<u32> = Vec::new();
+    for n in 0..count {
+        fib.push(fibonacci(n as u32));
+    }
+    fib
 }
